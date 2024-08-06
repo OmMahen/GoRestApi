fce74a44ee6fc264e70731e25d39982c22e08f05
OmMahen <102027819+OmMahen@users.noreply.github.com>
2024-08-06 14:13:28 +0700

    Update db.go

--- db/db.go
+++ db/db.go
@@ -3,8 +3,6 @@ package db
 import (
 	"database/sql"
 
-	"github.com/OmMahen/GoRestApi/models"
-
 	"log"
 
 	_ "github.com/lib/pq"
@@ -23,28 +21,4 @@ func Init() {
 	if err = DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-var MovieList = []models.Movie{
-
-	{
-		Id:         "1",
-		Title:      "The Godfather",
-		Year:       "1972",
-		Category:   "Cime/Drama",
-		ImdbRating: "9.2/10",
-	},
-	{
-		Id:         "2",
-		Title:      "The English Patient",
-		Year:       "1996",
-		Category:   "Romance/Drama",
-		ImdbRating: "7.4/10",
-	}, {
-		Id:         "3",
-		Title:      "The Greate Gatsby",
-		Year:       "2013",
-		Category:   "Romance/Drama",
-		ImdbRating: "7.2/10",
-	},
 }
